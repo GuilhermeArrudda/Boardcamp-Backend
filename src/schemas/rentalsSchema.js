bacaf541bfe038113ff1741b6a1322f0ce06cd43
Marcio Cunha <marcioabcd11@gmail.com>
2022-03-10 10:33:29 -0300

    Preparando Review

--- src/schemas/rentalsSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const rentalsSchema = joi.object({
-    customerId: joi.number().integer().greater(0).required(),
-    gameId: joi.number().integer().greater(0).required(),
-    daysRented: joi.number().integer().greater(0).required()
-});
-
-export default rentalsSchema;
